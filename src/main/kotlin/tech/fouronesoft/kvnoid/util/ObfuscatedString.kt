48693e80027931465993202385d080cfb1e12838
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2026-02-23 19:18:55 -0500

    initial ObfuscatedString implementation

---
+++ src/main/kotlin/tech/fouronesoft/kvnoid/util/ObfuscatedString.kt
@@ -0,0 +1,80 @@
+package tech.fouronesoft.kvnoid.util
+
+import tech.fouronesoft.kvnoid.encryption.AESGCMKey
+
+/**
+ * A way to store String data in memory in an obfuscated way
+ */
+class ObfuscatedString(
+  initialValue: ByteArray = ByteArray(1),
+  overwriteInitialValueSource: Boolean = true,
+) {
+
+  private val encryptionKey: AESGCMKey = AESGCMKey.temporaryKey(size = 128)
+  private var value: ByteArray = ByteArray(0)
+
+  init {
+    setValue(newValue = initialValue, overwrite = overwriteInitialValueSource)
+  }
+
+  /**
+   * Store a new obfuscated value.
+   *
+   * @param newValue to store
+   * @param overwrite if newValue should be zeroed out after consumption (default `true`)
+   * @return this ObfuscatedString instance
+   */
+  fun setValue(newValue: ByteArray, overwrite: Boolean = true): ObfuscatedString {
+    this.value = this.encryptionKey.encrypt(newValue)
+    if (overwrite)
+      for (i in 0..<newValue.size) newValue[i] = 0.toByte()
+    return this
+  }
+
+  /**
+   * Get a ValueProvider, an AutoCloseable which temporarily exposes the plain unobfuscated value of
+   * the stored string.
+   *
+   * @return ObfuscatedString.ValueProvider
+   */
+  fun getProvider(): ValueProvider {
+    val decryptedByteArray: ByteArray = this.encryptionKey.decrypt(this.value)
+    val charArray = CharArray(decryptedByteArray.size)
+
+    for (i in 0..<decryptedByteArray.size) {
+      charArray[i] = decryptedByteArray[i].toInt().toChar()
+      decryptedByteArray[i] = 0
+    }
+
+    return ValueProvider(charArray)
+  }
+
+  /**
+   * Temporarily exposes the plain unobfuscated value of an obfuscated string.
+   * When it is close()d it will zero out the value it has stored in memory.
+   *
+   * @property get retrieve the value
+   * @property close zero out the value in memory
+   */
+  class ValueProvider(private val value: CharArray) : AutoCloseable {
+    private var closed: Boolean = false
+
+    /**
+     * Retrieves the unobfuscated value
+     *
+     * @throws RuntimeException if this provider is closed
+     */
+    fun get(): CharArray {
+      require(!this.closed) { "This provider has been closed and its value can no longer be accessed" }
+      return this.value
+    }
+
+    /**
+     * Zeroes out the unobfuscated value and renders the instance useless
+     */
+    override fun close() {
+      for (i in 0..<value.size) value[i] = Char(0)
+      this.closed = true
+    }
+  }
+}
---
+++ src/test/kotlin/tech/fouronesoft/kvnoid/util/ObfuscatedStringTest.kt
@@ -0,0 +1,67 @@
+package tech.fouronesoft.kvnoid.util
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertContentEquals
+
+class ObfuscatedStringTest {
+
+  @Test
+  fun test_noArgsFedToConstructor() {
+    val obfuscatedString = ObfuscatedString()
+    val provider: ObfuscatedString.ValueProvider = obfuscatedString.getProvider()
+    assertContentEquals(
+      CharArray(1),
+      provider.get())
+    provider.close()
+    assertThrows<RuntimeException>("This provider has been closed and its value can no longer be accessed") {
+      provider.get()
+    }
+  }
+
+  @Test
+  fun test_sameValueProvided() {
+    val obfuscatedString = ObfuscatedString(
+      initialValue = "abcde".toByteArray(DataSerializationUtils.STANDARD_CHARSET))
+
+    obfuscatedString.getProvider().use { provider ->
+      assertContentEquals("abcde".toCharArray(), provider.get())
+      provider.close()
+    }
+  }
+
+  @Test
+  fun test_constructor_overwritesSourceArray() {
+    val initialArray: ByteArray = "abcde".toByteArray(Charsets.UTF_8)
+    ObfuscatedString(initialValue = initialArray)
+    assertContentEquals(ByteArray(5), initialArray)
+  }
+
+  @Test
+  fun test_constructor_preservesSourceArray() {
+    val initialArray: ByteArray = "abcde".toByteArray(Charsets.UTF_8)
+    ObfuscatedString(initialValue = initialArray, overwriteInitialValueSource = false)
+    assertContentEquals("abcde".toByteArray(Charsets.UTF_8), initialArray)
+  }
+
+  @Test
+  fun test_setValue_overwritesSourceArray() {
+    val obfuscatedString = ObfuscatedString()
+    val initialArray: ByteArray = "abcde".toByteArray(Charsets.UTF_8)
+    obfuscatedString.setValue(newValue = initialArray)
+
+    assertContentEquals(ByteArray(5), initialArray)
+    obfuscatedString.getProvider().use { provider ->
+      assertContentEquals("abcde".toCharArray(), provider.get())
+    }
+  }
+
+  @Test
+  fun test_setValue_preservesSourceArray() {
+    val obfuscatedString = ObfuscatedString()
+    val initialArray: ByteArray = "abcde".toByteArray(Charsets.UTF_8)
+    obfuscatedString.setValue(newValue = initialArray, overwrite = false)
+    assertContentEquals("abcde".toByteArray(Charsets.UTF_8), initialArray)
+  }
+
+}
